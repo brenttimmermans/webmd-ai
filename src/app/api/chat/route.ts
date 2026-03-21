e05c6f36485cacbb11d57d176db4f1196bc42c25
Brent Timmermans <brenttimmermans7@gmail.com>
2026-03-21 20:37:07 +0100

    bugfix: Call correct agent :)

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,15 +1,18 @@
-import { handleChatStream } from "@mastra/ai-sdk";
-import { createUIMessageStreamResponse } from "ai";
-import { mastra } from "@/mastra";
+import { handleChatStream } from '@mastra/ai-sdk';
+import { createUIMessageStreamResponse } from 'ai';
+import { mastra } from '@/mastra';
+import { traigeAgent } from '@/mastra/agents/triage-agent';
 
 export async function POST(req: Request) {
   const params = await req.json();
   const stream = await handleChatStream({
     mastra,
-    agentId: "weather-agent",
+    agentId: traigeAgent.id,
     params,
   });
   return createUIMessageStreamResponse({
-    stream: stream as Parameters<typeof createUIMessageStreamResponse>[0]["stream"],
+    stream: stream as Parameters<
+      typeof createUIMessageStreamResponse
+    >[0]['stream'],
   });
 }

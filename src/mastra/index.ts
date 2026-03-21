5fa42608c27b16c4f027534e4b57cd8f3c750ce4
Brent Timmermans <brenttimmermans7@gmail.com>
2026-03-21 21:08:25 +0100

    chore: Clean up weather agent

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,12 +1,9 @@
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { traigeAgent } from './agents/triage-agent';
-import { weatherAgent } from './agents/weather-agent';
-import { weatherWorkflow } from './workflows/weather-workflow';
 
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow },
-  agents: { weatherAgent, traigeAgent },
+  agents: { traigeAgent },
   logger: new PinoLogger({
     name: 'Mastra',
     level: 'info',
